696d81679784bfd32ac059c44369a4f07fd8fd4e
Mikhail <99481254+DCFApixels@users.noreply.github.com>
2024-02-22 17:04:54 +0800

    Update ThreadRunner.cs

--- src/ThreadRunner.cs
+++ src/ThreadRunner.cs
@@ -1,7 +1,6 @@
 using DCFApixels.DragonECS.ClassicThreadsInternal;
 using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Threading;
 
 namespace DCFApixels.DragonECS
